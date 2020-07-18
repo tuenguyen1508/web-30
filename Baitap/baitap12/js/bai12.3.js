b0efbe713f3563a191281164649bf1cb5b2e9e24
tuent158998 <tuenguyen15081997@gmail.com>
2020-07-18 17:06:59 +0700

    sua baitap12

--- Baitap/baitap12/js/bai12.3.js
+++ Baitap/baitap12/js/bai12.3.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	$('.menu li').hover(function() {
 		$(this).find('ul:first').slideDown(1000);
 	}, function() {
-		$(this).find('ul:first').hide(1000);
+		$(this).find('ul:first').hide();
 	});
 });
